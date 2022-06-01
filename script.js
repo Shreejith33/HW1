aa5a888a932746fc47fcc655500870d895837b36
Shreejith33 <93464358+Shreejith33@users.noreply.github.com>
2022-05-31 18:33:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var title = "My first console project"
+console.log(title)
+var subtitle = "Shreejith"
+console.log(subtitle)
+var number1 = "15"
+console.log(number1)
+var number2 = "5"
+console.log(number2)
+var number3 = "50"
+console.log(number3)
+var number4 = "2"
+console.log(number4)
